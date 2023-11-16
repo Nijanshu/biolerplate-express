5adb0a8db0f0a7bd8bb239c5db83704ea30629c3
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-11-16 18:23:58 +0530

    sendfile

--- myApp.js
+++ myApp.js
@@ -7,8 +7,12 @@ let app = express();
 console.log("Hello World")
 
 
-app.get('/', function(req, res){
-    res.send("Hello Express")
+// app.get('/', function(req, res){
+//     res.send("Hello Express")
+// })
+
+app.get('/',(req,res)=>{
+    res.sendFile('./views/index.html',{root:__dirname});
 })
 
 
